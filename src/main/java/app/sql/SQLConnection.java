e632dd145a2dd98601ee4f242ea6ce48b673994d
QuentinDAMMAN <quentindamman59@gmail.com>
2021-02-11 11:53:27 +0100

    crud modele

--- src/main/java/app/sql/SQLConnection.java
+++ src/main/java/app/sql/SQLConnection.java
@@ -5,16 +5,14 @@ import java.sql.DriverManager;
 import java.util.Properties;
 
 public class SQLConnection {
-public static Connection con = null;
-	
+	public static Connection con = null;
+
 	public static void connect() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Properties appProps = new Properties();
 			appProps.load(SQLConnection.class.getClassLoader().getResourceAsStream("db.properties"));
-			con = DriverManager.getConnection(
-					appProps.getProperty("url"), 
-					appProps.getProperty("username"),
+			con = DriverManager.getConnection(appProps.getProperty("url"), appProps.getProperty("username"),
 					appProps.getProperty("password"));
 			System.out.println("Connection Operationnal");
 		} catch (Exception e) {
